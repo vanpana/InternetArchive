9ad471e2be7ac31d85344d410fb00fef5f61cbe0
vanpana <van.panaite@gmail.com>
2017-10-18 19:47:43 +0300

    Tkinter
    Design and some button functionality
    Modified some base functions to generate .txt files, then, if needed, generate .html files.

--- download_helper.py
+++ download_helper.py
@@ -1,11 +1,15 @@
+import webbrowser
+from pathlib import Path
+from tkinter import *
 from urllib.request import urlopen
+
 
 import datetime
 import calendar
 import re
 import os
 
-
+from os.path import isfile, join
 from pip._vendor import requests
 
 def getTodayFolderName():
@@ -14,7 +18,18 @@ def getTodayFolderName():
 
 def getTodayString():
     now = datetime.datetime.now()
-    return calendar.day_abbr[now.weekday()] + ", " + str(now.day) + "." + str(now.month) + "." + str(now.year)
+    return calendar.day_abbr[now.weekday()] + "," + str(now.day) + "." + str(now.month) + "." + str(now.year)
+
+def getArchiveList():
+    mypath = "archive/"
+    templist = [f for f in os.listdir(mypath) if isfile(join(mypath, f))]
+
+    myarchive = []
+    for link in templist:
+        if ".txt" in link:
+            myarchive.append(link.strip(".txt"))
+
+    return myarchive
 
 def getHTMLFile(url, filename):
     '''
@@ -122,16 +137,38 @@ def getParsedData(url, no_of_items = 10):
     images = getXMLData(temp_file_name, "image", no_of_items)
     pubdate = getXMLData(temp_file_name, "pubdate", no_of_items)
 
-    # TODO: Cleanup
+    os.remove("temp.xml")
 
     return titles, links, descriptions, images, pubdate
+
+def writeParsedDataToFile(url, filename, no_of_items=10):
+    titles, links, descriptions, images, pubdate = getParsedData(url, no_of_items)
+
+    with open(filename, "w") as file:
+        for index in range(0, len(titles)):
+            file.write(titles[index] + "\n" + links[index] + "\n" + descriptions[index] + "\n" + \
+                       images[index] + "\n" + pubdate[index]+ "\n")
+
+def readParsedDataFromFile(filename):
+    titles, links, descriptions, images, pubdate = [], [], [], [], []
+    with open(filename, "r") as file:
+        for line in file:
+            titles.append(line.strip("\n"))
+            links.append(file.readline().strip("\n"))
+            descriptions.append(file.readline().strip("\n"))
+            images.append(file.readline().strip("\n"))
+            pubdate.append(file.readline().strip("\n"))
+
+    return titles, links, descriptions, images, pubdate
+
+
 
 #-------------- HTML Generation --------------#
 
 def generateSignature(archive_date, source_url):
     string = "<center><h1><font color=\"#415274\">ABC World News Archive</font></h1>\n"
     string += "<h2><font color=\"#415274\">" + archive_date + "</font></h2>\n"
-    string += "<img src=\"abclogo.png\">\n"
+    string += "<img src=\"../abclogo.gif\">\n"
     string += "<p><b>Source: </b>" + "<a href=" + source_url + ">" + source_url + "</a></p>\n"
     string += "<p><b>Archivist:</b> YOUR NAME GOES HERE</p></center>\n<hr>\n"
 
@@ -149,8 +186,8 @@ def generateStory(numberInQueue, title, link, description, image, date):
     return string
 
 
-def generateMyHTML(archive_date, filename, source_url=getWorldURL("http://www.abc.net.au/news/feeds/rss/")):
-    titles, links, descriptions, images, pubdate = getParsedData("http://www.abc.net.au/news/feeds/rss/")
+def generateMyHTML(archive_date, source_filename, dest_filename, source_url=getWorldURL("http://www.abc.net.au/news/feeds/rss/")):
+    titles, links, descriptions, images, pubdate = readParsedDataFromFile(source_filename)
 
     string = "<!DOCTYPE html>\n<html>\n<head>\n<body bgcolor=\"#E6E6FA\">\n"
     string += generateSignature(archive_date, source_url)
@@ -160,22 +197,91 @@ def generateMyHTML(archive_date, filename, source_url=getWorldURL("http://www.ab
 
     string += "</body>\n</head>\n</html>\n"
 
-    with open(filename, "w") as file:
+    with open(dest_filename, "w") as file:
         file.write(string)
 
-#print(getWorldURL())
-# downloadWorldXML(getWorldURL(), "/Users/vanpana/Desktop/InternetArchive/world.xml")
 
-# print(getTitlesXML("/Users/vanpana/Desktop/InternetArchive/world.xml", 2))
 
-# print(getXMLData("/Users/vanpana/Desktop/InternetArchive/world.xml", "title", 4))
-# print(getXMLData("/Users/vanpana/Desktop/InternetArchive/world.xml", "link", 4))
-# print(getXMLData("/Users/vanpana/Desktop/InternetArchive/world.xml", "description", 4))
-# print(getXMLData("/Users/vanpana/Desktop/InternetArchive/world.xml", "image", 4))
-# print(getXMLData("/Users/vanpana/Desktop/InternetArchive/world.xml", "pubdate", 4))
-# print(getTodayString())
+# writeParsedDataToFile("http://www.abc.net.au/news/feeds/rss/", "archive/Wed, 18.10.2017.txt")
+# print(readParsedDataFromFile("archive/Wed, 18.10.2017.txt"))
+# generateMyHTML(getTodayString(), "archive/Wed, 18.10.2017.txt", "archive/firstgenerated.html")
 
-generateMyHTML(getTodayString(), "firstgenerated.html")
+#-------------- Tkinter --------------#
+
+# Func #
+def getSelection():
+    selection = getArchiveList()[lbox.curselection()[0]]
+    if selection == "latest":
+        selection = getTodayString()
+    return selection
+
+def extractNews():
+    if lbox.curselection() == ():
+        infotext.config(text="No item selected!")
+    else:
+        selection = getSelection()
+        generateMyHTML(selection, "archive/" + selection + ".txt", "archive/" + selection + ".html")
+        infotext.config(text="Archive for " + selection + " extracted!")
+
+def displayNews():
+    if lbox.curselection() == ():
+        infotext.config(text="No item selected!")
+    else:
+        selection = getSelection()
+        filename = os.path.dirname(os.path.realpath(__file__)) + "/archive/" + selection + ".html"
+
+        if not Path(filename).is_file():
+            infotext.config(text="Archive for " + selection + " not extracted yet!")
+        else:
+            filename = "file://" + os.path.dirname(os.path.realpath(__file__)) + "/archive/" + selection + ".html"
+            webbrowser.open(filename)
+
+def fetchLatestNews():
+    writeParsedDataToFile("http://www.abc.net.au/news/feeds/rss/", "archive/" + getTodayString() + ".txt")
+
+# UI #
+print(getArchiveList())
+root = Tk()
+root.title("ABC News Archive")
+
+logoimg = PhotoImage(file="abclogo.gif")
+# w1 = Label(root, image=logo).pack(side="top")
+# w2 = Label(root,
+#            justify=LEFT,
+#            padx = 10,
+#            text="da\ndadadada\nfdsafas").pack(side="bottom left")
+# w3 = Label(root,
+#            justify=LEFT,
+#            padx = 10,
+#            text="fdsaga\ndfvnfgnfa\nfytryrtfas").pack(side="bottom right")
+
+
+
+logo = Label(root, image=logoimg).pack(side="top")
+
+lbox = Listbox(root)
+
+extract = Button(root, text="Extract news from archive", command=extractNews)
+display = Button(root, text="Display news from archive", command=displayNews)
+latest = Button(root, text="Fetch latest news")
+infotext = Label(text="Welcome to the archive!")
+
+lbox.pack()
+extract.pack()
+display.pack()
+latest.pack()
+infotext.pack()
+
+for item in getArchiveList():
+    if item != getTodayString():
+        lbox.insert(END, item)
+lbox.insert(END,"latest")
+
+root.mainloop()
+
+
+
+
 
 #writeToFile("http://www.abc.net.au/news/feeds/rss/", "/Users/vanpana/Desktop/InternetArchive/abc.html")
 
